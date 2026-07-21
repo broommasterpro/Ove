970c40cadc01241096170bace73ffebf765317a8
Arshpreet Kaur <kaur.arshpreet7606@gmail.com>
2026-07-22 03:56:18 +0530

    Spelling lesson

--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -22,10 +22,10 @@ public:
         std::vector<Token> tokens;
         std::string buf;
         //fix -> add expected TokenType
-        while (peak().has_value()) {
-            if (std::isalpha(peak().value())) {
+        while (peek().has_value()) {
+            if (std::isalpha(peek().value())) {
                 buf.push_back(consume());
-                while (peak().has_value() && std::isalnum(peak().value())) {
+                while (peek().has_value() && std::isalnum(peek().value())) {
                     buf.push_back(consume());
                 }
                 if (buf == "exit") {
@@ -38,21 +38,21 @@ public:
                     exit(EXIT_FAILURE);
                 }
             }
-            else if (std::isdigit(peak().value())) {
+            else if (std::isdigit(peek().value())) {
                 buf.push_back(consume());
-                while (peak().has_value() && std::isdigit(peak().value())) {
+                while (peek().has_value() && std::isdigit(peek().value())) {
                     buf.push_back(consume());
                 }
                 tokens.push_back({ .type = TokenType::int_lit, .value = buf });
                 buf.clear();
                 continue;
             }
-            else if (peak().value() == ';') {
+            else if (peek().value() == ';') {
                 consume();
                 tokens.push_back({ .type = TokenType::semi });
                 continue;
             }
-            else if (std::isspace(peak().value())) {
+            else if (std::isspace(peek().value())) {
                 consume();
                 continue;
             }
@@ -66,7 +66,7 @@ public:
     }
 
 private:
-    [[nodiscard]] inline std::optional<char> peak(int ahead = 1) const
+    [[nodiscard]] inline std::optional<char> peek(int ahead = 1) const
     {
         if (m_index + ahead > m_src.length()) {
             return {};
